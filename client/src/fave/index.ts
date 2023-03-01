fd3327dc508ef3c87c8ad35b1abe3b50bb53a378
Jason Godesky <jefgodesky@gmail.com>
2023-03-01 14:41:49 -0500

    Create star fave

--- client/src/fave/index.ts
+++ client/src/fave/index.ts
@@ -1,20 +1,18 @@
 import axios from 'axios'
-import { create } from 'unobtrusive-dom'
 
 const selector = 'article[data-pokedex]'
 
-const getStar = async (): Promise<Document> => {
+const getStar = async (): Promise<string> => {
   const res = await axios('/star.svg')
-  const parser = new DOMParser()
-  return parser.parseFromString(res.data, 'image/svg+xml')
+  return res.data
 }
 
 const init = async (nodes: Element[]): Promise<void> => {
   const star = await getStar()
   for (const node of nodes) {
     const img = Array.from(node.children).filter(tag => tag.tagName === 'IMG')[0]
-    const children = [star.cloneNode(true) as HTMLElement]
-    const btn = create({ tag: 'button', children })
+    const btn = document.createElement('button')
+    btn.innerHTML = star
     if (img !== undefined) img.after(btn)
   }
 }
