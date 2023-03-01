1e738cb7677a16613d4160e7a9d4ad40a4110da2
Jason Godesky <jefgodesky@gmail.com>
2023-03-01 12:55:06 -0500

    Fix /by-name (and /by-pokedex, too, really)

--- server/src/routes/home.ts
+++ server/src/routes/home.ts
@@ -1,27 +1,26 @@
 import express, { Request, Response } from 'express'
-import Pokemon from '../interfaces/Pokemon'
 import sortAlphabetically from '../interfaces/Pokemon/sort-alphabetically.js'
 import sortByPokedex from '../interfaces/Pokemon/sort-by-pokedex.js'
 import data from '../pokemon.json' assert { type: 'json' }
 
 const router = express.Router()
-const pokemon = data as Pokemon[]
-const byName = sortAlphabetically(pokemon)
-const byPokedex = sortByPokedex(pokemon)
 
 // GET /
 router.get('/', (req: Request, res: Response) => {
-  res.render('home', { pokemon: byPokedex, sort: 'pokedex' })
+  const pokemon = sortByPokedex(data)
+  res.render('home', { pokemon, sort: 'pokedex' })
 })
 
 // GET /by-name
 router.get('/by-name', (req: Request, res: Response) => {
-  res.render('home', { pokemon: byName, sort: 'name' })
+  const pokemon = sortAlphabetically(data)
+  res.render('home', { pokemon, sort: 'name' })
 })
 
 // GET /by-pokedex
 router.get('/by-pokedex', (req: Request, res: Response) => {
-  res.render('home', { pokemon: byPokedex, sort: 'pokedex' })
+  const pokemon = sortByPokedex(data)
+  res.render('home', { pokemon, sort: 'pokedex' })
 })
 
 export default router
