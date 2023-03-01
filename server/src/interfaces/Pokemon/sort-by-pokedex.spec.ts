2852e3127fce43a9dc84592d8e5ff269caf51bfe
Jason Godesky <jefgodesky@gmail.com>
2023-03-01 11:07:08 -0500

    Add new method: sortByPokedex

---
+++ server/src/interfaces/Pokemon/sort-by-pokedex.spec.ts
@@ -0,0 +1,17 @@
+import 'mocha'
+import { expect } from 'chai'
+import sortByPokedex from './sort-by-pokedex'
+
+describe('sortByPokedex', () => {
+  const pokemon = [
+    { pokedex: 7, name: 'Squirtle', types: ['water'] },
+    { pokedex: 4, name: 'Charmander', types: ['fire'] },
+    { pokedex: 1, name: 'Bulbasaur', types: ['grass', 'poison'] },
+    { pokedex: 10, name: 'Caterpie', types: ['bug'] }
+  ]
+
+  it('sorts Pokemon in ascending order by Pokédex', () => {
+    const actual = sortByPokedex(pokemon)
+    expect(actual.map(pokemon => pokemon.name).join(' ')).to.equal('Bulbasaur Charmander Squirtle Caterpie')
+  })
+})
