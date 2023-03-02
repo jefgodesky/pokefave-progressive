c27fb821ccaa42b6b6b1b510ae5d805cb0fa70f7
Jason Godesky <jefgodesky@gmail.com>
2023-03-02 10:23:01 -0500

    localStorage mock: Use Map instead of object

--- client/src/fakes/local-storage.ts
+++ client/src/fakes/local-storage.ts
@@ -1,14 +1,14 @@
 export default function (): Storage {
-  let store: { [key: string]: string } = {}
+  const store = new Map()
 
   return {
-    length: 0,
-    setItem (key: string, val: string): void { store[key] = val },
-    getItem (key: string): any { return key in store ? store[key] : null },
-    removeItem (key: string): void { if (key in store) delete store[key] },
-    clear (): void { store = {} },
+    length: store.size,
+    setItem (key: string, val: string): void { store.set(key, val) },
+    getItem (key: string): any { return store.get(key) ?? null },
+    removeItem (key: string): void { store.delete(key) },
+    clear (): void { store.clear() },
     key (index: number): string {
-      const keys = Object.keys(store)
+      const keys = Array.from(store.keys())
       return keys[index] ?? ''
     }
   }
