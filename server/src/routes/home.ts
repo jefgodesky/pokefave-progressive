8736ccd6b4347b86a3844556a7cfc8de3b60af48
Jason Godesky <jefgodesky@gmail.com>
2023-03-02 13:21:05 -0500

    Pass faves only query param through to front-end

--- server/src/routes/home.ts
+++ server/src/routes/home.ts
@@ -1,26 +1,30 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import sortAlphabetically from '../interfaces/Pokemon/sort-alphabetically.js'
 import sortByPokedex from '../interfaces/Pokemon/sort-by-pokedex.js'
 import data from '../pokemon.json' assert { type: 'json' }
 
 const router = express.Router()
 
+const getFavesOnly = (req: Request): boolean => {
+  return req.query.hasOwnProperty('faves')
+}
+
 // GET /
 router.get('/', (req: Request, res: Response) => {
   const pokemon = sortByPokedex(data)
-  res.render('home', { pokemon, sort: 'pokedex' })
+  res.render('home', { pokemon, sort: 'pokedex', favesOnly: getFavesOnly(req) })
 })
 
 // GET /by-name
 router.get('/by-name', (req: Request, res: Response) => {
   const pokemon = sortAlphabetically(data)
-  res.render('home', { pokemon, sort: 'name' })
+  res.render('home', { pokemon, sort: 'name', favesOnly: getFavesOnly(req) })
 })
 
 // GET /by-pokedex
 router.get('/by-pokedex', (req: Request, res: Response) => {
   const pokemon = sortByPokedex(data)
-  res.render('home', { pokemon, sort: 'pokedex' })
+  res.render('home', { pokemon, sort: 'pokedex', favesOnly: getFavesOnly(req) })
 })
 
 export default router
