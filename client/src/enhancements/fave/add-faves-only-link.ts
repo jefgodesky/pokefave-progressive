b9dab202db34058ddc85f01c89cccaac3ebc1830
Jason Godesky <jefgodesky@gmail.com>
2023-03-02 13:21:24 -0500

    Client-side enhancement: faves: addFavesOnlyLink: Respond to faves only

--- client/src/enhancements/fave/add-faves-only-link.ts
+++ client/src/enhancements/fave/add-faves-only-link.ts
@@ -1,10 +1,11 @@
-import { create } from 'unobtrusive-dom/esm'
+import { create, hasClass } from 'unobtrusive-dom/esm'
 
 export default function (): void {
+  const classes = hasClass(document.body, 'faves-only') ? ['active'] : []
   const nav = document.querySelector('main > nav')
   if (nav === null) return
   const base = nav.querySelector('a[data-fave-base]')
   const href = base?.getAttribute('href') ?? '/by-name'
-  const link = create({ tag: 'a', attrs: { href: `${href}?faves=true`, 'data-faves-only': 'true' }, text: 'Faves Only' })
+  const link = create({ tag: 'a', attrs: { href: `${href}?faves=true`, 'data-faves-only': 'true' }, classes, text: 'Faves Only' })
   nav.appendChild(link)
 }
